65cd71640e7065d627977e161d799646bba1de26
ZhouYue <1007215586@qq.com>
2019-02-05 23:16:13 +0800

    入口类Entrance

---
+++ src/com/zy/basic_oops/Person.java
@@ -0,0 +1,20 @@
+package com.zy.basic_oops;
+
+public class Person {
+    // 成员变量   修饰符 数据类型  数据引用  赋值
+    public String name;
+    public int age;
+    public double height;
+
+
+    // 成员方法
+    public void eat() {
+
+    }
+    public void work() {
+
+    }
+    public void sleep() {
+
+    }
+}
