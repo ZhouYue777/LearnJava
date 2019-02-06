78a329b3d3f26c71563cba68e032b71b1b8d6b8b
ZhouYue <1007215586@qq.com>
2019-02-06 21:55:17 +0800

    打印范围内奇数作业

---
+++ src/com/zy/array/ArrayOdd.java
@@ -0,0 +1,20 @@
+package com.zy.array;
+//
+public class ArrayOdd {
+    ArrayOdd(int maxnum) {
+        int[] odds = new int[maxnum/2 + 1];
+        for (int i = 0; i < odds.length ; i++) {
+            odds[i] = 2*i + 1;
+        }
+        for(int numbers : odds) {
+            System.out.println(numbers);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayOdd test = new ArrayOdd(999);
+    }
+}
+
+
+
