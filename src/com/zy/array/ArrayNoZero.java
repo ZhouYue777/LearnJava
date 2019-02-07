2cc8606a09d982bfbd0973fef565539e82009ce7
ZhouYue <1007215586@qq.com>
2019-02-07 16:19:39 +0800

    去掉数组中的0 2019.2.7

--- src/com/zy/array/ArrayNoZero.java
+++ src/com/zy/array/ArrayNoZero.java
@@ -1,7 +1,8 @@
 package com.zy.array;
 // 用于去掉数组中的0 元素
 // 思路1:计算非0元素的个数，算出新数组的长度。添加进入新数组
-// 思路2:找出0元素的位置。记录这些位置
+// 优化 完成后 可arrayBefore = null 释放内存  垃圾回收器GC可以释放
+// index++ 可以写成arrayAfter[index++]  先取值在自增
 public class ArrayNoZero {
     ArrayNoZero(int[] arrayBefore) {
         int count = 0;
