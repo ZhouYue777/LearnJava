aacfe8da549250b91575d2179c43740277c0c181
ZhouYue <1007215586@qq.com>
2019-02-07 11:08:55 +0800

    最大值最小值平均值 2019.2.7

--- src/com/zy/array/ArrayMaxMinAvg.java
+++ src/com/zy/array/ArrayMaxMinAvg.java
@@ -3,20 +3,36 @@ package com.zy.array;
 public class ArrayMaxMinAvg {
     int max;
     int min;
-    int avg;
+    double avg;
+    int sum;
     ArrayMaxMinAvg(int[] arrayA) {
+//        int max = arrayA[0];   定义在这里边，为局部变量，外边不好调用。改为成员变量。
+//        int min = arrayA[0];
+//        int avg;
+//        int sum;
         max = arrayA[0];
         min = arrayA[0];
-        for (int i = 1; i < arrayA.length ; i++) {
+
+        for (int i = 0; i < arrayA.length ; i++) {
             if (arrayA[i] > max) {
                 max = arrayA[i];
             }
             if (arrayA[i] < min) {
                 min = arrayA[i];
             }
+            sum = sum + arrayA[i];
         }
+        avg = (double)sum/arrayA.length;
+    }
 
 
+    public static void main(String[] args) {
+        ArrayMaxMinAvg test = new ArrayMaxMinAvg(new int[]{3,3,1,6,4,3,2});
+        System.out.println("最小值为" + test.min);
+        System.out.println("最大值为" + test.max);
+        System.out.println("平均值为" + test.avg);
 
     }
+
+
 }
